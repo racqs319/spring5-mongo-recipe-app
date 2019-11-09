c7eedec7798634140f6855a538c1141242b1ea0b
Racquel Cases <racquel.cases@hotmail.com>
2019-11-09 09:51:04 +0800

    Restore integration test

--- src/test/java/net/casesr/recipeapp/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/net/casesr/recipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -4,26 +4,38 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Optional;
 
+import net.casesr.recipeapp.bootstrap.RecipeBootstrap;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import net.casesr.recipeapp.domain.UnitOfMeasure;
 
-@Disabled
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@DataMongoTest
 class UnitOfMeasureRepositoryIT {
 	
 	@Autowired
 	UnitOfMeasureRepository unitOfMeasureRepository;
 
+	@Autowired
+	CategoryRepository categoryRepository;
+
+	@Autowired
+	RecipeRepository recipeRepository;
+
 	@BeforeEach
 	void setUp() throws Exception {
+		recipeRepository.deleteAll();
+		unitOfMeasureRepository.deleteAll();
+		categoryRepository.deleteAll();
+
+		RecipeBootstrap recipeBootstrap = new RecipeBootstrap(categoryRepository,
+				unitOfMeasureRepository, recipeRepository);
+		recipeBootstrap.onApplicationEvent(null);
 	}
 
 	@Test
